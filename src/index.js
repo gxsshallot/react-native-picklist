6cbb7af53efb3c6331b1ebbca13d587716a10e0c
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-07-04 15:13:15 +0800

    添加导出的index文件。

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import PickList from './PickList';
+import DefaultPickListRow, * as PickListImage from './DefaultPickListRow';
+import PickListCell from './PickListCell';
+import PickListBottomBar from './PickListBottomBar';
+import PickListShowAllCell from './PickListShowAllCell';
+import PickListTitleLine from './PickListTitleLine';
+
+export default PickList;
+
+export {
+    DefaultPickListRow,
+    PickListImage,
+    PickListCell,
+    PickListBottomBar,
+    PickListShowAllCell,
+    PickListTitleLine,
+};
